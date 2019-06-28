0c7f225e0a5f74b88506daee1a6f268afcd88c58
Clara Yip <clara.yip1@gmail.com>
2019-06-28 01:49:24 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -12,24 +12,24 @@ def second_challenge
       :hero => {
         :name => "Romeo"
         :age => "15"
+        :status => "alive"
       },
-      :hero_friends => [
-        {
-          :name => "Benvolio",
-          :age => "17",
-          :attitude => "worried"
-        },
-        {
-          :name => "Mercutio"
-          :age => "18"
-          :attitude => "hot-headed"
-        }
-      ]
+      :hero_friends => []
     }, 
     :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
+      :patriarch => {
+        :name => "Lord Capulet"
+        :age => "50"
+      },
+      :matriarch => {
+        :name => "Lady Capulet"
+        :age => "51"
+      },
+      :heroine => {
+        :name => "Juliet"
+        :age => "15"
+        :staus => "alive"
+      },
       :heroine_friends => []
       }
   }
