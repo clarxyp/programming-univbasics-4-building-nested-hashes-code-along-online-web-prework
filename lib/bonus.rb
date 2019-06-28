4c811aa97c07ad3c20a2febda3ef71b3773d8560
Clara Yip <clara.yip1@gmail.com>
2019-06-28 02:15:33 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -7,6 +7,7 @@ def bonus
       :hero_friends => [
          {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Steven", age: "30", attitude: "confused"}
+         ]
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
